03e0fcd4113c751d1b17a49f65f8a22c6900a3b5
shao <bailang426@163.com>
2017-07-03 16:38:10 +0800

    Initial commit

---
+++ app/src/main/java/com/example/shao/javathink_4/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.shao.javathink_4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 这个project主要是用来练习java基础的，通过JavaLibrary来练习java编程思想4
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ javalibrary/src/main/java/com/example/CallBack/CallBack.java
@@ -0,0 +1,10 @@
+package com.example.CallBack;
+/**
+ * 回调接口
+ * @author shao
+ *result 小李知道答案后 调用此方法通知小王
+ */
+public interface CallBack {
+	public void sovle(String result);
+
+}
