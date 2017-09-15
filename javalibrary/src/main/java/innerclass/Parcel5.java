6263b60e122e00ab4839262b8fdd17abc70bccf0
bailangsyc <741510290@qq.com>
2017-09-15 10:12:26 +0800

    10.5 在方法和作用域内的内部类

--- javalibrary/src/main/java/innerclass/Parcel5.java
+++ javalibrary/src/main/java/innerclass/Parcel5.java
@@ -4,6 +4,7 @@ import sun.swing.plaf.synth.DefaultSynthStyle;
 
 /**在方法和作用域内的内部类
  * 10.5
+ *
  * 在方法的作用域内（而不是其他类的作用域内）创建一个完整的类，被称作局部内部类
  * PDestination是方法destination(String s)中的一个类，相当于时一个局部变量，所以在方法外时无法访问的
  * Created by root on 17-9-14.
